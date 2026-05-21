12aa2bbc8240883a465b84a10ab2fe68c43a5e31
AyushAggarwal215 <ayushaggarwal2168@gmail.com>
2026-05-21 15:31:14 +0530

    question number added

--- sliding window/lc_question.cpp
+++ sliding window/lc_question.cpp
@@ -1,5 +1,5 @@
 /*
-Frequency of the Most Frequent Element
+QUESTION 1838: Frequency of the Most Frequent Element
 
 The frequency of an element is the number of times it occurs in an array.
 You are given an integer array nums and an integer k. In one operation, you can choose an index of nums and increment the element at that index by 1.
