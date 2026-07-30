a1e1888236714481ca83084df2d9a01a0d7157d9
AyushAggarwal215 <ayushaggarwal2168@gmail.com>
2026-07-31 01:06:21 +0530

    one more way to solve

--- dp/maximum_sum_non_adjacent_numbers.cpp
+++ dp/maximum_sum_non_adjacent_numbers.cpp
@@ -91,12 +91,27 @@ int solvetab(vector<int> &nums){
     }
     return 0;
 }
+int solve(vector<int> &nums){
+    int n = nums.size();
+    int prev1 = nums[0];
+    int prev2 = 0;
+    for(int i=1; i<n; i++){
+        int incl = prev2+nums[i];
+        int excl = prev1;
+        int ans = max(incl, excl);
+        prev2 = prev1;
+        prev1 = ans;
+    }
+    return prev1;
+
+}
 int maximumNonAdjacentSum(vector<int> &nums){
     // Write your code here.
     int n = nums.size();
     // int ans = solverec(nums, n-1);
     // return ans;
     // vector<int> dp(n,-1);
-    int ans = solvetab(nums);
+    // int ans = solvetab(nums);
+    int ans = solve(nums);
     return ans;
 }
